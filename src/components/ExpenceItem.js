fe89d33efd785e63a4b518f8fc138fe7c870826a
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-07-12 15:00:44 +0300

    add css

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,4 +1,11 @@
 function ExpenceItem() {
-    return <h1>Expence item!</h1>
+    return (
+        <div>
+            <div>July 12th 2023</div>
+            <h2>Car</h2>
+            <div>$234.30</div>
+        </div>
+    );
+
 }
 export default ExpenceItem
