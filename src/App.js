bfc35c054da2fdbb4c28de7e298acce120105f52
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-07-12 14:50:59 +0300

    create first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+
+import ExpenceItem from "./components/ExpenceItem";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <h2>Let's get started 2!</h2>
+      <ExpenceItem></ExpenceItem>
     </div>
   );
 }
---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,4 @@
+function ExpenceItem() {
+    return <h1>Expence item!</h1>
+}
+export default ExpenceItem
