888afd08d66d702c55bbe3a27bd593279677bcd3
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-07-12 15:19:46 +0300

    add dummy dinamic data

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,11 +1,18 @@
-function ExpenceItem() {
-    return (
-        <div>
-            <div>July 12th 2023</div>
-            <h2>Car</h2>
-            <div>$234.30</div>
-        </div>
-    );
+import "./ExpenceItem.css";
 
+function ExpenceItem() {
+    const date = new Date();
+    const item = 'Car';
+    const price = '3000';
+
+  return (
+    <div className="expense-item">
+      <div>{date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{item}</h2>
+        <div className="expense-item__price">${price}</div>
+      </div>
+    </div>
+  );
 }
-export default ExpenceItem
+export default ExpenceItem;
