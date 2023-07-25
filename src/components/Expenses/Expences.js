99e872d87b403659bc9018677e489732b6d231b7
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-07-25 10:04:52 +0300

    Update Expences.js

--- src/components/Expenses/Expences.js
+++ src/components/Expenses/Expences.js
@@ -13,6 +13,18 @@ const Expenses = ({ items }) => {
   const filteredExpenses = items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenceItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
 
   return (
     <div>
@@ -21,14 +33,7 @@ const Expenses = ({ items }) => {
           selected={filteredYear}
           onChangeFilter={filteredChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenceItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
